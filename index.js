1d5152322f01448755a5c5bc37e923ab1d5214de
Justin John Mathews <justin@amt.in>
2013-06-18 15:38:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,6 @@ var amtTimeZone = function () {
     var https = require('https'),
         querystring = require('querystring'),
 
-         mergeObjects = function (existedObj, passedObj) {
-            var mergedObj = {};
-            for (var name in existedObj) { mergedObj[name] = existedObj[attrname]; }
-            for (var name in passedObj) { mergedObj[name] = passedObj[attrname]; }
-            return mergedObj;
-         },
-
         /**
          * Makes request to Google Time Zone API and passes result to a cb
          *
@@ -45,10 +38,9 @@ var amtTimeZone = function () {
          * @param {String} lat, required
          * @param {String} lng, required
          * @param {Function} cb, required
-         * @param {Object} options, optional
          * @api public
          */
-        getTimeZone = function (lat, lng, cb, opts) {
+        getTimeZone = function (lat, lng, cb) {
 
             if (!lat || !lng) {
                 return cb(new Error("Time zoner requires a location cordinates."));
@@ -56,15 +48,11 @@ var amtTimeZone = function () {
 
             var timestamp = Math.round((new Date()).getTime() / 1000),
                 options = {sensor: false, timestamp:timestamp, location:lat+','+lng},
-                params;
-            if (opts) {
-                options = mergeObjects(options, opts);
-            }
-             params = {
-                hostname: 'maps.googleapis.com',
-                port: 443,
-                path: '/maps/api/timezone/json?' + querystring.stringify(options)
-            };
+                params = {
+                    hostname: 'maps.googleapis.com',
+                    port: 443,
+                   path: '/maps/api/timezone/json?' + querystring.stringify(options)
+                };
 
             return request(params, cb);
         };
